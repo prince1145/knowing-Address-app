7dd4593115b82fb7f4c5c39488f55bc4c410eeec
Bhuva prince <102386337+prince1145@users.noreply.github.com>
2022-06-15 15:43:44 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.hickerwatch;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    LocationManager locationManager;
+    LocationListener locationListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                UpgateLoctionInfo(location);
+            }
+        };
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                Location lastknowLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (lastknowLocation != null){
+                    UpgateLoctionInfo(lastknowLocation);
+                }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startListinening();
+            }
+    }
+    public void startListinening(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    public void UpgateLoctionInfo(Location location){
+        TextView latTextview = findViewById(R.id.lattTextview);
+        TextView longTextview = findViewById(R.id.longTextview);
+        TextView accTextview = findViewById(R.id.accTextview);
+        TextView altTextview = findViewById(R.id.altTextview);
+        TextView addressTextview = findViewById(R.id.addressTextview);
+
+        latTextview.setText("Latitude: "+ Double.toString(location.getLatitude()));
+        longTextview.setText("Longitude: "+ Double.toString(location.getLongitude()));
+        accTextview.setText("Accuracy: "+ Double.toString(location.getAccuracy()));
+        altTextview.setText("latitude: "+ Double.toString(location.getLatitude()));
+
+        String address = "Could not find address :(";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if(listAddresses != null && listAddresses.size()>0){
+                address = "Address: \n";
+                if(listAddresses.get(0).getThoroughfare() != null){
+                    address += listAddresses.get(0).getThoroughfare() + " \n";
+                }
+                if(listAddresses.get(0).getLocality() != null){
+                    address += listAddresses.get(0).getLocality() + ",";
+                }
+                if(listAddresses.get(0).getPostalCode() != null){
+                    address += listAddresses.get(0).getPostalCode() + ",";
+                }
+                if(listAddresses.get(0).getAdminArea() != null){
+                    address += listAddresses.get(0).getAdminArea() + ",";
+                }
+                if(listAddresses.get(0).getCountryName() != null){
+                    address += listAddresses.get(0).getCountryName() ;
+                }
+
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        addressTextview.setText(address);
+    }
+}
